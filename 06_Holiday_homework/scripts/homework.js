f417283d0c4b23e3a2b15c5ed5a5f28c81aaf21e
zombidust <kalo.stoykov@gmail.com>
2014-12-22 18:35:00 +0200

    task_4

--- 06_Holiday_homework/scripts/homework.js
+++ 06_Holiday_homework/scripts/homework.js
@@ -6,4 +6,13 @@ $(document).ready(function() {
 
 	//task_3
 	console.log($("#col1 > p").text());
+
+	//task_4
+	var list = $("#menu-top-level-menu");
+	var link = $("<a/>").text("NEW BUTTON");
+	link.attr("href", "#");
+
+	var item = $("<li/>");
+	item.append(link);
+	list.append(item);
 })
