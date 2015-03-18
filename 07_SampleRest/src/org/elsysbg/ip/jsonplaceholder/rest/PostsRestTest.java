6a10a27d273ad25730c3ebf2e8b99b7f948959b1
zombidust <kalo.stoykov@gmail.com>
2015-03-18 16:33:31 +0200

    Real unit test for PostsRest

--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/rest/PostsRestTest.java
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/rest/PostsRestTest.java
@@ -1,19 +1,29 @@
 package org.elsysbg.ip.jsonplaceholder.rest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
+import org.elsysbg.ip.jsonplaceholder.ServicesTestHelper;
 import org.elsysbg.ip.jsonplaceholder.model.Post;
-import org.junit.After;
-import org.junit.AfterClass;
+import org.elsysbg.ip.jsonplaceholder.service.PostsService;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
+/**
+ * For unit testing you can use JUnit or TestNG
+ */
 public class PostsRestTest {
 	private PostsRest postsRest;
+	private PostsService postsService;
 
 	@Before
 	public void setUp() throws Exception {
+		postsService = Mockito.mock(PostsService.class);
+		
+		ServicesTestHelper.setPostsService(postsService);
 		postsRest = new PostsRest();
 	}
 
@@ -22,9 +32,25 @@ public class PostsRestTest {
 		final Post post = new Post();
 		post.setTitle("hello");
 		post.setBody("world");
-		final Post result = postsRest.createPost(post);
-		assertNotEquals(0, result.getId());
+		
+		Mockito.when(postsService.createPost(post)).
+			then(new Answer<Post>() {
+			@Override
+			public Post answer(InvocationOnMock invocation) throws Throwable {
+				final Post result = invocation.getArgumentAt(0, Post.class);
+				result.setId(1);
+				return result;
+			}
+		});
+		final Post result =
+			postsRest.createPost(post);
+		
+		Mockito.verify(postsService).createPost(post);
+		 
+		Mockito.verifyNoMoreInteractions(postsService);
+
+		assertNotNull(result.getUser());
 		assertEquals("hello@world", result.getUser().getEmail());
-		assertEquals("secret", result.getUser().getPassword());		
+		assertEquals("secret", result.getUser().getPassword());
 	}
 }
