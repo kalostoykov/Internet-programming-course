e70bbc3e37420b6ae314d69b97884c1eb190a045
Kaloyan Stoykov <zombidust@users.noreply.github.com>
2015-03-18 19:24:26 +0200

    Delete ServiceTestHelper.java

--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/ServiceTestHelper.java
+++
@@ -1,9 +0,0 @@
-package org.elsysbg.ip.jsonplaceholder;
-
-import org.elsysbg.ip.jsonplaceholder.service.PostsService;
-
-public class ServiceTestHelper {
-	public static void setPostsService(PostsService postsService) {
-		Services.setPostsService(postsService);
-	}
-}
