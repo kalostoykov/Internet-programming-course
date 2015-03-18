6f69af96783b7621af01bf8c82dcf1a1ce381315
zombidust <kalo.stoykov@gmail.com>
2015-03-18 19:05:48 +0200

    Implemented REST: get post by ID

--- 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/rest/PostsRest.java
+++ 07_SampleRest/src/org/elsysbg/ip/jsonplaceholder/rest/PostsRest.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -34,7 +35,11 @@ public class PostsRest {
 	public List<Post> getPosts() {
 		return postsService.getPosts();
 	}
-	public Post getPost(long postId) {
+
+	@GET
+	@Path("/{postId}")
+	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public Post getPost(@PathParam("postId") long postId) {
 		return postsService.getPost(postId);
 	}
 
